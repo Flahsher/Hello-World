7798a9ef6068d28425381f825a3e05633e8d5f55
Richard Zhao <32103323+Flahsher@users.noreply.github.com>
2017-10-10 16:55:22 +0800

    Add files via upload

---
+++ 数据实验(输出结果).cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<float.h>
+int main(){
+	int x=2.2;
+	short y=0x7FFF;
+	short z=0xFFFF;
+	unsigned short w=0xFFFF;
+	float u=0.177;
+	printf("x=%d\n",x);
+	printf("y=%d\n",y);
+	printf("z=%d\n",z);
+	printf("w=%d\n",w);
+	printf("u=%f\n",u);
+	printf("DBL_MAX=%le",DBL_MAX);
+	
+	return 0;
+}
